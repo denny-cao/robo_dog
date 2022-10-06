698322b9ea646507109f4010044978e11ceb93a8
Denny Cao <8008925@philasd.org>
2022-10-06 10:46:44 -0400

    set up methods for gps traversal

--- robo_dog.ino
+++ robo_dog.ino
@@ -4,6 +4,7 @@
 #include <TMRpcm.h>
 #include <SPI.h>
 #include <SD.h>
+#include <math.h>
 
 // MOTORS L298N
 
@@ -34,6 +35,16 @@ TMRpcm tmrpcm;
 #define SPEAKER_PIN 10
 
 tmmmrpcm.speakerPin = 10;
+
+// GPS Coordinates
+#define SENSITIVITY = .01;
+float cur_longitude;
+float cur_latitude;
+
+float goal_longitude;
+float goal_latitude;
+
+float bearing;
 
 void setup() {
   Serial.begin(115200); 
@@ -134,6 +145,52 @@ void left() {
   // Set speed (0, 255)
   analogWrite(A_SPEED_CTRL_PIN, 100);
   analogWrite(B_SPEED_CTRL_PIN, 0);
+}
+
+void updateCurrentCoords() {
+  // From Android phone coordinates, update current coordinate variables
+  ;
+}
+
+void updateGoalCoords() {
+  /* 
+    From Android phone, get new destination to reach
+    Idea: Google Maps or different map API to get "corners", or
+          positions where a change in direction is required
+    Simple Approach: Set goal coord once, and straight path.
+                     Follow path until obstacle, then avoid and
+                     try to stay on line
+  */
+  ;
+}
+
+void detectOrientation() {
+  /*
+    Solution 1: Move forwards and see what happens to coordinates to
+                detect orientation
+      - Problem: What to do if both long and lat increase? Then its on an angle...
+       - Maybe use trig to compute angle?
+      - Problem: GPS Coordinate inaccuracy
+    Solution 2: Pull bearing from Android phone as well
+      - https://stackoverflow.com/questions/41250064/android-how-to-get-gps-bearing
+  */
+  foward();
+  delay(1000); // Delay 1000 ms before stopping to get accurate change in reading
+}
+
+void path() {
+  float lat_diff = goal_latitude - cur_latitude;
+  float long_diff = goal_longitude - cur_longitude;
+
+  float dist = sqrt(lat_diff * lat_diff + long_diff * long_diff);
+
+  if (dist > SENSITIVITY) {
+    /*
+      Goal: Create way to get correct bearing to travel at; probably use trig
+            to compute angle and then figure out how to go a specific angle
+            by tweaking speed of motors
+    */
+  }
 }
 
 void avoid() {
